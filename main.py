7cf796205fe0fc50da5eb9bce8a272d4815a8007
KylerORourke <74610460+KylerORourke@users.noreply.github.com>
2023-12-18 16:38:33 -0600

    added config file on run

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from ossapi import Ossapi, UserLookupKey, GameMode, RankingType
-from discord.ext import commands
 import discord
-import requests
-from discord import SlashCommand, SlashContext
+from discord.ext import commands, tasks
+import certifi 
 
-client_id = 'None'
+import json 
+import os 
 
-client_secret = 'None'
+if os.path.exists(os.getcwd() + "/config.json"):
+    
+    with open("./config.json") as f:
+        configData = json.load(f)
+
+else: 
+    configTemplate = {"Token": "", "Prefix": "!"}
+    
+    with open(os.getcwd() + "/config.json", "w+") as f: 
+        json.dump(configTemplate, f)
+
+token = configData["Token"]
+prefix = configData["Prefix"]
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime, time
+# import interactions
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
+
+client_id = 28923
+
+client_secret = 'pv6WR4HNI4yKlL7vWuvtDx2Owy8qUf2aoNpsT55v'
 
 api = Ossapi(client_id, client_secret)
 
@@ -23,24 +45,36 @@ print(top50.ranking[0].user.username)
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+scheduler = AsyncIOScheduler() 
 
-slash = SlashCommand(bot, sync_commands=True)  #
 @bot.command()
 async def topscorer(ctx):
     # Replace with actual method to fetch top scorer from Osu! API
    
     # Format the data in a user-friendly way
-    response = (f"Top 25: {top50.ranking[0].user.username}")
+    response = (f"Top 25: {top50.ranking[0].user.username}\n\t\t\t {top50.ranking[1].user.username}")
     await ctx.send(response)
 
 
 @bot.command()
-async def top(ctx):
-    
-   requests.get("https://osu.ppy.sh/api/v2/users?ids%5B%5D=1") 
-   
-#response = (f"Your top play: {top.scores_best_count()}")
-bot.run('TOKEN')
+async def dailypp(ctx): 
+    response = (f"Daily leader: {top50.ranking[0].user.username}")
+    await ctx.send(response)
 
+async def send_leaderboard():
+    channel = bot.get_channel('1186354377745780767')  # Replace with your channel ID
+    leaderboard = await dailypp()
+    await channel.send(leaderboard)
+
+    # Schedule the task (e.g., everyday at 10 AM)
+scheduler.add_job(send_leaderboard, 'cron', hour=12, minute=28, second=1)
+
+
+scheduler.start()
+
+
+
+
+bot.run(token)
 
 
