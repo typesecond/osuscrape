6ada68544df910eb25e9efb71fed2039edca315b
KylerORourke <74610460+KylerORourke@users.noreply.github.com>
2023-12-17 20:54:58 -0600

    add

--- botly.py
+++ botly.py
@@ -5,7 +5,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def topscorer(ctx):
     # Replace with actual method to fetch top scorer from Osu! API
-    top_scorer_data = ossapi.get_top_scorer()  
+    top_scorer_data = Ossapi.get_top_scorer()  
     # Format the data in a user-friendly way
     response = f"Top Scorer: {top_scorer_data['username']} with {top_scorer_data['score']} points"
     await ctx.send(response)
